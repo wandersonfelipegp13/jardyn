ac665d1456a4d76b2a9c07b8e1bf309d1357fa28
Wanderson <wandersonfelipeplus13@gmail.com>
2021-08-02 21:43:14 -0300

    Adicionando comentários na funções do menu.

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,7 +1,8 @@
+/* Exibe menu no modo mobile */
 function abre() {
     document.getElementById("menu-id").style.display = "inherit";
 }
-
+/* Esconde menu no modo mobile */
 function fecha() {
     document.getElementById("menu-id").style.display = "none";
 }
